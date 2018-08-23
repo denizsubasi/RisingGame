c9bba8fc15a341d57abb065e5f6e69159dadfd4c
Ali Kılıç <alikilicx7@gmail.com>
2018-08-23 21:56:36 +0300

    initialize commit

---
+++ Assets/movingbox.cs
@@ -0,0 +1,65 @@
+﻿
+using UnityEngine;
+
+public class movingbox : MonoBehaviour {
+    public float hiz = 100f;
+    float a = 0.1f;
+    public int leftOrRight;
+
+    void Start(){
+      // leftOrRight= getRandomNuber();
+    }
+    void Update () {
+
+        transform.position =new Vector3(transform.position.x+a,0,0);
+        if ((int)transform.position.x == 3) { a = -0.1f;
+            Debug.Log("şuan 3 de");
+        }
+        if ((int)transform.position.x == -3)
+        {
+            a = 0.1f;
+            Debug.Log("şuan -3 de");
+
+        }
+
+        /*
+                if (leftOrRight == 1)
+                {
+                    saghareket();
+                }
+                else
+                {
+                    solahareket();
+                }
+
+                Vector3 defaultVector = new Vector3(-3f, 0f, 0f);
+                Vector3 defaultVector2 = new Vector3(3f, 0f, 0f);
+                if (transform.position==defaultVector)
+                {
+                    saghareket();
+                }
+               else if (transform.position==defaultVector2)
+                {
+                    solahareket();
+                }
+            }
+
+            int getRandomNuber()
+            {
+                return Random.Range(0, 1);
+            }
+            void saghareket()
+            {
+                Vector3 defaultVector = transform.position;
+                Vector3 defaultVector2 = new Vector3(0.2f, 0f, 0f);
+                transform.position = defaultVector + defaultVector2;
+                */
+    }
+    void solahareket()
+    {
+        Vector3 defaultVector = transform.position;
+        Vector3 defaultVector2 = new Vector3(0.2f, 0f, 0f);
+        transform.position = defaultVector - defaultVector2;
+    }
+}
+
---
+++ Assets/players.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class players : MonoBehaviour {
+
+    public float yukselmegucu = 10f;
+    public Rigidbody2D rb;
+    
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0) )
+        {
+            rb.velocity = Vector2.up * yukselmegucu;
+        }
+	}
+}
